68829f11f1fcdaed957a4adac56f74dc27bc1d6d
Stekovaya <68231194+Stekovaya@users.noreply.github.com>
2020-08-06 19:47:56 +0900

    Create index.js

---
+++ .circleci/index.js
@@ -0,0 +1,17 @@
+var fs = require("fs");
+var toml = require("toml");
+var tmp = toml.parse(fs.readFileSync("index.toml") + "");
+for(var val in tmp){
+  fs.writeFileSync("main.oba",val.code);
+  var sp = require("child_process").spawnSync("mono",["obrya.exe","main.oba"]);
+  console.log("-----" + val.name);
+  sp.stdout.on("data",e=>{
+    process.stdout.write(e);
+  });
+  sp.on("close",exitc=>{
+    if(exitc != 0){
+      console.log("\r\nError");
+      process.exit(1);
+    }
+  });
+}
